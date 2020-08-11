4f292a0d75e08e68d06357533e2cbd171feef349
TheDarkDnKTv <x22dnk@hotmail.com>
2020-08-11 17:49:58 +0300

    Creative tab fix

--- src/main/java/gregtechmod/api/util/GT_CreativeTab.java
+++ src/main/java/gregtechmod/api/util/GT_CreativeTab.java
@@ -1,20 +1,24 @@
 package gregtechmod.api.util;
 
 import gregtechmod.api.enums.GT_Items;
-import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 
 public class GT_CreativeTab extends CreativeTabs {
 
 	public GT_CreativeTab() {
 		super("GregTech");
-		LanguageRegistry.instance().addStringLocalization("itemGroup.GregTech", "GregTech Intergalactical");
 	}
 	
 	@Override
     public ItemStack getIconItemStack() {
-        return GT_Items.Tool_Cheat.getUndamaged(1, new ItemStack(Block.blockIron, 1));
+        return GT_Items.Tool_Cheat.getUndamaged(1, new ItemStack(Blocks.iron_block, 1));
     }
+
+	@Override
+	public Item getTabIconItem() {
+		return null;
+	}
 }
