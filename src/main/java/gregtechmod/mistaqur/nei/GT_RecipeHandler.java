7307f15b5e1cc89d488e89f894937de1e67910a8
TheDarkDnKTv <x22dnk@hotmail.com>
2021-03-03 05:06:05 +0200

    Now GT's NEI handler support liquids

--- src/main/java/gregtechmod/mistaqur/nei/GT_RecipeHandler.java
+++ src/main/java/gregtechmod/mistaqur/nei/GT_RecipeHandler.java
@@ -1,25 +1,31 @@
 package gregtechmod.mistaqur.nei;
 
 import gregtechmod.api.GregTech_API;
+import gregtechmod.api.enums.GT_Items;
 import gregtechmod.api.recipe.ChancedOutput;
 import gregtechmod.api.recipe.Ingredient;
 import gregtechmod.api.recipe.Recipe;
 import gregtechmod.api.recipe.RecipeMap;
-import gregtechmod.api.util.GT_OreDictUnificator;
 import gregtechmod.api.util.GT_Utility;
 
+import static codechicken.lib.gui.GuiDraw.changeTexture;
+
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.fluids.FluidStack;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.lwjgl.opengl.GL11;
 
+import codechicken.lib.gui.GuiDraw;
+import codechicken.nei.NEIClientConfig;
 import codechicken.nei.PositionedStack;
 import codechicken.nei.api.API;
 import codechicken.nei.recipe.GuiRecipe;
@@ -41,6 +47,9 @@ public abstract class GT_RecipeHandler extends TemplateRecipeHandler {
 	public class CachedGT_Recipe extends CachedRecipe {
 		public List<PositionedStack> products;
 		public List<PositionedStack> resources;
+		public List<PositionedFluidStack> fluidProducts;
+		public List<PositionedFluidStack> fluidResources;
+		
 		public List<Integer> nonConsumables;
 		public Map<Integer, Integer> chanced;
 		
@@ -49,6 +58,8 @@ public abstract class GT_RecipeHandler extends TemplateRecipeHandler {
 		public CachedGT_Recipe(Recipe recipe) {
 			resources 		= new ArrayList<>();
 			products 		= new ArrayList<>();
+			fluidResources	= new ArrayList<>();
+			fluidProducts 	= new ArrayList<>();
 			nonConsumables 	= new ArrayList<>();
 			chanced			= new HashMap<>();
 			
@@ -79,6 +90,20 @@ public abstract class GT_RecipeHandler extends TemplateRecipeHandler {
 				this.chanced.put(GT_Utility.stackToInt(output.getStack()), output.getChance());
 			}
 			
+			List<FluidStack> fluidInputs = recipe.getFluidInputs();
+			for (int i = 0; i < fluidInputs.size(); i++) {
+				Pair<Integer, Integer> offsets = getFluidInputAligment(i);
+				FluidStack fluid = fluidInputs.get(i);
+				fluidResources.add(new PositionedFluidStack(fluid, offsets.getKey(), offsets.getValue()));
+			}
+			
+			List<FluidStack> fluidOutputs = recipe.getFluidOutputs();
+			for (int i = 0; i < fluidOutputs.size(); i++) {
+				Pair<Integer, Integer> offsets = getFluidOutputAligment(i + offset);
+				FluidStack fluid = fluidOutputs.get(i);
+				fluidProducts.add(new PositionedFluidStack(fluid, offsets.getKey(), offsets.getValue()));
+			}
+			
 			mDuration = recipe.getDuration();
 			mEUt = recipe.getEUt();
 		}
@@ -105,6 +130,14 @@ public abstract class GT_RecipeHandler extends TemplateRecipeHandler {
 		protected Pair<Integer, Integer> getOutputAligment(int itemIdx) {
 			return Pair.of((18 * itemIdx), 54);
 		}
+		
+		protected Pair<Integer, Integer> getFluidInputAligment(int itemIdx) {
+			return Pair.of(18 * itemIdx, 0);
+		}
+		
+		protected Pair<Integer, Integer> getFluidOutputAligment(int itemIdx) {
+			return Pair.of((18 * itemIdx), 54);
+		}
 	}
 	
 	@Override
@@ -121,22 +154,95 @@ public abstract class GT_RecipeHandler extends TemplateRecipeHandler {
 	
 	public abstract CachedGT_Recipe getRecipe(Recipe irecipe);
 	
+	protected void provideTooltip(GuiRecipe gui, ItemStack stack, List<String> currenttip, CachedGT_Recipe recipe, Point relMouse) {
+		for (PositionedFluidStack fluid : recipe.fluidResources) {
+			if (fluid.fits(relMouse)) {
+				currenttip.add(fluid.fluid.getLocalizedName());
+				currenttip.add(EnumChatFormatting.GRAY.toString() + I18n.format("metatileentity.fluid.amount", GT_Utility.parseNumberToString(fluid.fluid.amount)));
+			}
+		}
+		
+		for (PositionedFluidStack fluid : recipe.fluidProducts) {
+			if (fluid.fits(relMouse)) {
+				currenttip.add(fluid.fluid.getLocalizedName());
+				currenttip.add(EnumChatFormatting.GRAY.toString() + I18n.format("metatileentity.fluid.amount", GT_Utility.parseNumberToString(fluid.fluid.amount)));
+			}
+		}
+	}
+	
 	@Override
 	public List<String> handleItemTooltip(GuiRecipe gui, ItemStack stack, List<String> currenttip, int recipeId) {
+		super.handleItemTooltip(gui, stack, currenttip, recipeId);
 		CachedRecipe r = arecipes.get(recipeId);
 		
 		if (r instanceof CachedGT_Recipe) {
 			CachedGT_Recipe recipe = (CachedGT_Recipe) r;
-			Integer stackInt = GT_Utility.stackToInt(stack);
-			if (recipe.nonConsumables.contains(stackInt)) {
-				currenttip.add(I18n.format("item.non_consumable.tooltip"));
-			} else if ((stackInt = recipe.chanced.get(stackInt)) != null) {
-				currenttip.add(I18n.format("item.chanced.tooltip", GT_Utility.parseChanceString(stackInt)));
+			if (GT_Utility.isStackValid(stack)) {
+				Integer stackInt = GT_Utility.stackToInt(stack);
+				if (recipe.nonConsumables.contains(stackInt)) {
+					currenttip.add(I18n.format("item.non_consumable.tooltip"));
+				} else if ((stackInt = recipe.chanced.get(stackInt)) != null) {
+					currenttip.add(I18n.format("item.chanced.tooltip", GT_Utility.parseChanceString(stackInt)));
+				}
+			} else {
+				Point mouse = GuiDraw.getMousePosition();
+				Point offset = gui.getRecipePosition(recipeId);
+				Point relMouse = new Point(mouse.x - (gui.width - 176) / 2 - offset.x, mouse.y - (gui.height - 166) / 2 - offset.y);
+				provideTooltip(gui, stack, currenttip, recipe, relMouse);
 			}
 		}
 		
 		return currenttip;
 	}
+	
+    @Override
+    public boolean keyTyped(GuiRecipe gui, char keyChar, int keyCode, int recipe) {
+        if (keyCode == NEIClientConfig.getKeyBinding("gui.recipe")) {
+            if (transferFluid(gui, recipe, false)) {
+                return true;
+            }
+        } else if (keyCode == NEIClientConfig.getKeyBinding("gui.usage")) {
+            if (transferFluid(gui, recipe, true)) {
+                return true;
+            }
+        }
+        return super.keyTyped(gui, keyChar, keyCode, recipe);
+    }
+    
+    @Override
+    public boolean mouseClicked(GuiRecipe gui, int button, int recipe) {
+        if (button == 0) {
+            if (transferFluid(gui, recipe, false)) {
+                return true;
+            }
+        } else if (button == 1) {
+            if (transferFluid(gui, recipe, true)) {
+                return true;
+            }
+        }
+        return super.mouseClicked(gui, button, recipe);
+    }
+	
+	protected boolean transferFluid(GuiRecipe guiRecipe, int recipe, boolean usage) {
+        CachedGT_Recipe crecipe = (CachedGT_Recipe) this.arecipes.get(recipe);
+        Point mousepos = GuiDraw.getMousePosition();
+        Point offset = guiRecipe.getRecipePosition(recipe);
+        Point relMouse = new Point(mousepos.x - (guiRecipe.width - 176) / 2 - offset.x, mousepos.y - (guiRecipe.height - 166) / 2 - offset.y);
+        
+        for (PositionedFluidStack stack : crecipe.fluidResources) {
+            if (stack.fits(relMouse)) {
+                return stack.transfer(usage);
+            }
+        }
+        
+        for (PositionedFluidStack stack : crecipe.fluidProducts) {
+            if (stack.fits(relMouse)) {
+                return stack.transfer(usage);
+            }
+        }
+        
+        return false;
+    }
 	
 	@Override
     public void drawBackground(int recipe) {
@@ -146,32 +252,53 @@ public abstract class GT_RecipeHandler extends TemplateRecipeHandler {
     }
     
 	@Override
+    public void drawForeground(int recipe) {
+        GL11.glColor4f(1, 1, 1, 1);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        changeTexture(getGuiTexture());
+        drawFluidStacks(recipe);
+        drawExtras(recipe);
+    }
+	
+	public void drawFluidStacks(int recipe) {
+		CachedGT_Recipe cached = (CachedGT_Recipe) arecipes.get(recipe);
+		for (PositionedFluidStack fluid : cached.fluidResources)
+			fluid.draw();
+		for (PositionedFluidStack fluid : cached.fluidProducts)
+			fluid.draw();
+	}
+	
+	@Override
 	public int recipiesPerPage() {
 		return 1;
 	}
 	
 	@Override
-	public void loadTransferRects() {
-		
-	}
-	
-	@Override
 	public void loadCraftingRecipes(String outputId, Object... results) {
-		if(outputId.equals(getRecipeId())) {
+		if (outputId.equals("liquid") && results[0] instanceof FluidStack && ((FluidStack) results[0]).getFluid() != null) {
+			this.loadCraftingRecipes((FluidStack) results[0]);
+		} else if(outputId.equals(getRecipeId())) {
 			for (Recipe irecipe : getRecipeList().getRecipes()) {
 				arecipes.add(getRecipe(irecipe));
 			}
-		} else {
-			super.loadCraftingRecipes(outputId, results);
-		}
+		} else super.loadCraftingRecipes(outputId, results);
 	}
+	
+    @Override
+    public void loadUsageRecipes(String inputId, Object... ingredients) {
+        if (inputId.equals("liquid") && ingredients[0] instanceof FluidStack && ((FluidStack) ingredients[0]).getFluid() != null) {
+            this.loadUsageRecipes((FluidStack) ingredients[0]);
+        } else {
+            super.loadUsageRecipes(inputId, ingredients);
+        }
+    }
 	
 	@Override
 	public void loadCraftingRecipes(ItemStack result) {
-		ItemStack tStack = GT_OreDictUnificator.get(true, result);
+		FluidStack fluid = GT_Utility.getFluidForFilledItem(result);
 		for (Recipe irecipe : getRecipeList().getRecipes()) {
 			CachedGT_Recipe recipe = getRecipe(irecipe);
-			if (recipe.contains(recipe.products,tStack) || recipe.contains(recipe.products,result)) {
+			if (recipe.contains(recipe.products,result) || irecipe.getFluidOutputs().contains(fluid)) {
 				arecipes.add(recipe);
 			}
 		}
@@ -179,15 +306,47 @@ public abstract class GT_RecipeHandler extends TemplateRecipeHandler {
 	
 	@Override
 	public void loadUsageRecipes(ItemStack ingredient) {
-		ItemStack tStack = GT_OreDictUnificator.get(false, ingredient);
+		FluidStack fluid = GT_Utility.getFluidForFilledItem(ingredient);
 		for (Recipe irecipe : getRecipeList().getRecipes()) {
 			CachedGT_Recipe recipe = getRecipe(irecipe);
-			if (recipe.contains(recipe.resources,tStack) || recipe.contains(recipe.resources,ingredient)) {
+			if (recipe.contains(recipe.resources,ingredient) || irecipe.getFluidInputs().contains(fluid)) {
 				arecipes.add(recipe);
 			}
 		}
 	}
 	
+    public void loadUsageRecipes(FluidStack ingredient) {
+    	for (Recipe irecipe : getRecipeList().getRecipes()) {
+			if (irecipe.getFluidInputs().contains(ingredient)) {
+				arecipes.add(getRecipe(irecipe));
+				continue;
+			}
+			
+			for (Ingredient ingr : irecipe.getInputs()) {
+				ItemStack cell = GT_Utility.fillFluidContainer(ingredient, GT_Items.Cell_Empty.get(1));
+				if (ingr.match(cell)) {
+					arecipes.add(getRecipe(irecipe));
+				}
+			}
+		}
+    }
+    
+    public void loadCraftingRecipes(FluidStack result) {
+		for (Recipe irecipe : getRecipeList().getRecipes()) {
+			if (irecipe.getFluidOutputs().contains(result)) {
+				arecipes.add(getRecipe(irecipe));
+				continue;
+			}
+			
+			for (ItemStack stack : irecipe.getAllOutputs()) {
+				FluidStack stack1 = GT_Utility.getFluidForFilledItem(stack);
+				if (stack1 != null && result.isFluidEqual(stack1)) {
+					arecipes.add(getRecipe(irecipe));
+				}
+			}
+		}
+    }
+    
 	public void drawText(int aX, int aY, String aString, int aColor, boolean aB) {
 		Minecraft.getMinecraft().fontRenderer.drawString(aString, aX, aY, aColor);
 	}
---
+++ src/main/java/gregtechmod/mistaqur/nei/PositionedFluidStack.java
@@ -0,0 +1,84 @@
+package gregtechmod.mistaqur.nei;
+
+import java.awt.Point;
+import java.awt.Rectangle;
+
+import org.lwjgl.opengl.GL11;
+
+import codechicken.lib.gui.GuiDraw;
+import codechicken.nei.recipe.GuiCraftingRecipe;
+import codechicken.nei.recipe.GuiUsageRecipe;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import gregtechmod.api.util.GT_Utility;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.util.IIcon;
+import net.minecraftforge.fluids.FluidStack;
+
+/**
+ * @author TheDarkDnKTv
+ *
+ */
+public class PositionedFluidStack {
+	
+	protected int x;
+	protected int y;
+	protected FluidStack fluid;
+	/**
+	 * 
+	 */
+	public PositionedFluidStack(FluidStack fluid, int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.fluid = fluid;
+	}
+	
+	public boolean transfer(boolean usage) {
+        if (GT_Utility.isFluidStackValid(fluid)) {
+            if (usage) {
+                if (!GuiUsageRecipe.openRecipeGui("liquid", new Object[] { fluid })) {
+                    return false;
+                }
+            } else {
+                if (!GuiCraftingRecipe.openRecipeGui("liquid", new Object[] { fluid })) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+	
+	@SideOnly(Side.CLIENT)
+	public void draw() {
+		if (GT_Utility.isFluidStackValid(fluid)) {
+			IIcon fluidIcon = fluid.getFluid().getIcon(fluid);
+			fluidIcon = fluidIcon != null ? fluidIcon : fluid.getFluid().getFlowingIcon();
+			fluidIcon = fluidIcon != null ? fluidIcon : fluid.getFluid().getStillIcon();
+			
+			GuiDraw.changeTexture(TextureMap.locationBlocksTexture);
+			int color = fluid.getFluid().getColor(fluid);
+			GL11.glColor3ub((byte) (color >> 16 & 0xFF), (byte) (color >> 8 & 0xFF), (byte) (color & 0xFF));
+			GL11.glDisable(GL11.GL_BLEND);
+			
+			double minU = fluidIcon.getMinU();
+            double maxU = fluidIcon.getMaxU();
+            double minV = fluidIcon.getMinV();
+            double maxV = fluidIcon.getMaxV();
+            
+            Tessellator.instance.startDrawingQuads();
+            Tessellator.instance.addVertexWithUV(x		, y + 16	, 0, maxU, minV);
+            Tessellator.instance.addVertexWithUV(x + 16	, y + 16	, 0, minU, minV);
+            Tessellator.instance.addVertexWithUV(x + 16	, y			, 0, minU, maxV);
+            Tessellator.instance.addVertexWithUV(x		, y			, 0, maxU, maxV);
+            Tessellator.instance.draw();
+            
+            GL11.glEnable(GL11.GL_BLEND);
+		}
+	}
+	
+	public boolean fits(Point point) {
+		return new Rectangle(x, y, 16, 16).contains(point);
+	}
+}
