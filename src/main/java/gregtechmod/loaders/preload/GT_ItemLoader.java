7fd09d3d7e7d3efdca0515b4d66bf1a68174c25d
CrossVas <cross.wanted.1.4>
2021-11-27 23:29:51 +0900

    Fix accidentally added import

--- src/main/java/gregtechmod/loaders/preload/GT_ItemLoader.java
+++ src/main/java/gregtechmod/loaders/preload/GT_ItemLoader.java
@@ -2,7 +2,6 @@ package gregtechmod.loaders.preload;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
-import forestry.api.core.IGameMode;
 import gregtechmod.GT_Mod;
 import gregtechmod.api.GregTech_API;
 import gregtechmod.api.enums.GT_Items;
